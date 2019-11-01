28677f63badfe8389b6419e64eeb9b28c1fd210f
Kelvin Law <kailaw@gmail.com>
2019-11-01 21:39:27 +0800

    added .gitingore and also .gitattributes to kotlin-jvm template

--- plopfile.js
+++ plopfile.js
@@ -34,7 +34,7 @@ module.exports = function(plop) {
                     base: 'templates/{{template}}',
                     templateFiles: '**/*',
                     globOptions: { dot: true },
-                    destination: './{{artifactId}}'
+                    destination: '{{artifactId}}'
                 }
             ]
         }        
